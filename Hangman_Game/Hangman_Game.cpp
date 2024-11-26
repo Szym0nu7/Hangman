523efb7cab4d3f7361523bd20860fae6946842eb
Szym0nu7 <91138890+Szym0nu7@users.noreply.github.com>
2024-11-26 12:03:28 +0100

    Basic setup, Start of work
    Set up game class along with the game loop. Planned out what to add and started working on player input and checking it.

--- Hangman_Game/Hangman_Game.cpp
+++ Hangman_Game/Hangman_Game.cpp
@@ -1,8 +1,73 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
+
+bool hasChar(string txt, char letter) {
+    if (txt.find(letter) < txt.size() && txt.find(letter) >= 0)
+        return true;
+    else
+        return false;
+}
+
+class HangmanGame {
+public:
+    HangmanGame() {};
+
+    void printGame() {
+        cout << endl;
+        for (int i = 0; i < word_to_guess.size(); i++) {
+            if (word_to_guess[i] != ' ')cout << "_";
+            else cout << word_to_guess[i];
+        }
+        cout << endl;
+    }
+
+    void playerInput() {
+        //print accessible letters
+        cout << "Choose letter or type in the answer: ";
+        getline(cin,player_input);
+    }
+
+    void checkPlayerInput() {
+        if (player_input == word_to_guess) {
+            //yay you win
+            cout << endl << "You guessed the word";
+        }
+        if (hasChar(word_to_guess,player_input[0])) {
+            //trigger letter
+            cout << endl << "You one letter";
+        }
+        else {
+            //progress the hanging proccess by one
+        }
+    }
+
+    void GameLoop() {
+        printGame();
+    //render rules and game
+    //choose guessed word
+        while (true) {
+            playerInput();
+            checkPlayerInput();
+            //check answer
+            printGame();
+            
+        }
+    //end game
+    };
+private:
+    string word_to_guess = "Oto wisielec";
+    string player_input;
+    vector<char> guessed_letters;
+    vector<char> letters_to_guess;
+    vector<char> letters_to_print;
+};
+
 int main()
 {
-    cout << "This is the start of the Hangman Game";
+    HangmanGame game;
+    game.GameLoop();
 }
 
